3ef619cb6cae04341fa29f3d41d050e9a1a10d05
iainharfield <iainharfield@users.noreply.github.com>
2016-10-12 17:34:22 +0100

    Initial testing

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+echo "Install.... $IAIN
+sudo apt-get update && sudo apt-get install -y iperf screen
---
+++ pcrf_configure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Configuring test PCRF....."
+echo 'server_private2='$server_private2 > /home/ubuntu/info
