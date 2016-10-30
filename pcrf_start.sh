07f7a3a2f0e7baebbc931e67c39bba216e265118
iainharfield <iainharfield@users.noreply.github.com>
2016-10-30 13:53:48 +0000

    Add files via upload

--- pcrf_start.sh
+++ pcrf_start.sh
@@ -8,5 +8,7 @@ echo "*******************************************"
 
 echo 'server_private2='$server_private2 > /home/ubuntu/info
 
+ls /opt/openbaton/scripts/
+
 cp /opt/openbaton/scripts/stresstest.sh ~/
 chmod +x ~/stresstest.sh
