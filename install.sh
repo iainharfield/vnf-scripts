6600875006a58c2c28741f9ed22e85e29a841a36
iainharfield <iainharfield@users.noreply.github.com>
2016-10-30 12:07:21 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ echo "*******************************************"
 
 sudo apt-get update && sudo apt-get install -y iperf screen
 sudo apt-get install -y stress
+
+cp stresstest ~/
+chmod +x stresstest
