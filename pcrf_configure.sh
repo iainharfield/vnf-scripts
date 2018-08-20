ff6ed472a25800994f3dd81d9ccd07385a757b8c
iainharfield <iainharfield@users.noreply.github.com>
2018-08-20 15:54:57 +0100

    Update pcrf_configure.sh

--- pcrf_configure.sh
+++ pcrf_configure.sh
@@ -6,4 +6,4 @@ echo " hostname:    $hostname"
 echo " private_floatingIp: $private_floatingIp "
 echo "*******************************************"
 
-# echo 'server_private2='$server_private2 > /home/ubuntu/info
+echo 'private='$private > /home/ubuntu/info-configure
