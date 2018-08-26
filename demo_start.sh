c9987c097dc044e1abf1289c4987c7f4a2499e0d
iainharfield <iainharfield@users.noreply.github.com>
2018-08-26 23:24:10 +0100

    Update demo_start.sh

--- demo_start.sh
+++ demo_start.sh
@@ -10,9 +10,8 @@ echo 'private_floatingIp='$private_floatingIp > /home/ubuntu/info
 
 ls /opt/openbaton/scripts/
 
-cp /opt/openbaton/scripts/stresstest.sh /home/ubuntu/
-chmod +x /home/ubuntu/stresstest.sh
 
-# install Streambase VNF Demo
-epadmin install node   --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
-epadmin servicename=A.ubuntu stop node
+
+# Start Streambase VNF Demo
+# epadmin install node   --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
+sudo --user=ubuntu epadmin servicename=A.ubuntu stop node
