ed6a5732d34672405b5bd16aebb89eab251f7b59
iainharfield <iainharfield@users.noreply.github.com>
2018-08-26 13:35:20 +0100

    Start StreamBase VNF

---
+++ demo_start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "*******************************************"
+echo "Start.... $IAIN"
+echo " networkName: $networkName"
+echo " hostname:    $hostname"
+echo " private_floatingIp: $private_floatingIp "
+echo "*******************************************"
+
+echo 'private_floatingIp='$private_floatingIp > /home/ubuntu/info
+
+ls /opt/openbaton/scripts/
+
+cp /opt/openbaton/scripts/stresstest.sh /home/ubuntu/
+chmod +x /home/ubuntu/stresstest.sh
+
+# install Streambase VNF Demo
+epadmin install node   --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
+
