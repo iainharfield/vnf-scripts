7e171da830e3436549f05c508b89e8715bc51d1e
iainharfield <iainharfield@users.noreply.github.com>
2018-08-26 13:37:02 +0100

    Update pcrf_start.sh

--- pcrf_start.sh
+++ pcrf_start.sh
@@ -13,6 +13,5 @@ ls /opt/openbaton/scripts/
 cp /opt/openbaton/scripts/stresstest.sh /home/ubuntu/
 chmod +x /home/ubuntu/stresstest.sh
 
-# install Streambase VNF Demo
-epadmin install node   --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
-
+# Start Streambase VNF Demo
+epadmin servicename=A.ubuntu stop node
