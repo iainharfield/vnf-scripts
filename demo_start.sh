b3ca36cf1dda1e1678b801b77de9d7148e6c1bb5
iainharfield <iainharfield@users.noreply.github.com>
2018-08-26 23:22:02 +0100

    Update demo_start.sh

--- demo_start.sh
+++ demo_start.sh
@@ -15,4 +15,4 @@ chmod +x /home/ubuntu/stresstest.sh
 
 # install Streambase VNF Demo
 epadmin install node   --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
-
+epadmin servicename=A.ubuntu stop node
