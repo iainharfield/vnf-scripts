af90dbdcb0a653687299c4071a98cf32facb5721
iainharfield <iainharfield@users.noreply.github.com>
2018-08-26 22:03:57 +0100

    Update demo_install.sh

--- demo_install.sh
+++ demo_install.sh
@@ -16,6 +16,8 @@ sudo /opt/openbaton/scripts.sh add $hostname $private_floatingIp
 # sudo apt-get update && sudo apt-get install -y iperf screen
 sudo apt-get install -y stress
 
+# CD to the user home (this script runs as root when launched by openbaton)
+cd /home/ubuntu
 
 # Get the demo Streambase build
 git clone https://github.com/iainharfield/sb-vnf.git
