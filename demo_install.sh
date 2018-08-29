0d954a542b682fe0d197d8a1d7db5369eb322b3f
iainharfield <iainharfield@users.noreply.github.com>
2018-08-29 21:01:49 +0100

    Update demo_install.sh

--- demo_install.sh
+++ demo_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+# The image expected to be built with stress application already installed
+#
 echo "*******************************************"
 echo "Instantiate.... $IAIN"
 echo " networkName: $networkName"
@@ -14,19 +16,14 @@ export TIBCO_EP_HOME=$STREAMBASE_HOME
 export PATH=/opt/tibco/sb-cep/10.3/jdk/bin:/opt/tibco/sb-cep/10.3/bin:/opt/tibco/sb-cep/10.3/distrib/tibco/bin:/opt/tibco/sb-cep/10.3/sdk/mvn/bin:$PATH
 
 # Configure /etc/hosts - relises on the script being downloaded, could there be a race condition here? 
-# Throw away error messages on first call to prevent this script from stopping.
+# Throw away error messages on first call to prevent this script from stopping unexpectedly.
 sudo /opt/openbaton/scripts/etchosts.sh add $hostname 2> /dev/null
 sudo /opt/openbaton/scripts.sh add $hostname $private_floatingIp
-
-# install stress application
-# sudo apt-get update && sudo apt-get install -y iperf screen
-# sudo apt-get install -y stress
 
 # CD to the user home (this script runs as root when launched by openbaton)
 cd /home/ubuntu
 
 # Get the demo Streambase build
-#sudo --user=ubuntu git clone https://github.com/iainharfield/sb-vnf.git
 git clone https://github.com/iainharfield/sb-vnf.git
 
 cp ./sb-vnf/vnfUtil.sh .
@@ -34,8 +31,6 @@ sudo chmod +x vnfUtil.sh
 cp /opt/openbaton/scripts/stresstest.sh .
 sudo chmod +x stresstest.sh
 
-
 # install Streambase VNF Demo (running as root, environment is not right)
-#sudo --user=ubuntu epadmin install node --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
 epadmin install node --application=./sb-vnf/deploy_nfvDemo-0.0.1-SNAPSHOT-ep-application.zip   --nodename=A.ubuntu --nodedirectory=/home/ubuntu/tmp/Nodedirs 
 
