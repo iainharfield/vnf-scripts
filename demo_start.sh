a67b6ff6bc74a32162a15fa9c2480633a3edaeaf
iainharfield <iainharfield@users.noreply.github.com>
2018-10-04 22:28:57 +0100

    Update demo_start.sh

--- demo_start.sh
+++ demo_start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "*******************************************"
-echo "  Start....                               *"
-echo "* Description : $Description              *"
-echo "* mqttBroker = $mqttBroker                *"
-echo "* mqttPublishDelay = $mqttPublishDelay    *"
+echo "  Start...."
+echo "* Description : $Description"
+echo "* mqttBroker = $mqttBroker"
+echo "* mqttPublishDelay = $mqttPublishDelay"
 echo "* networkName: $networkName"
 echo "* hostname:    $hostname"
-echo "* private_floatingIp: $private_floatingIp "
+echo "* private_floatingIp: $private_floatingIp"
 echo "*******************************************"
+
+export STREAMBASE_HOME=/opt/tibco/sb-cep/10.3
+export JAVA_HOME=$STREAMBASE_HOME/jdk
+export TIBCO_EP_HOME=$STREAMBASE_HOME
+export PATH=/opt/tibco/sb-cep/10.3/jdk/bin:/opt/tibco/sb-cep/10.3/bin:/opt/tibco/sb-cep/10.3/distrib/tibco/bin:/opt/tibco/sb-cep/10.3/sdk/mvn/bin:$PATH
 
 #Save floating point IP address for later
 echo 'private_floatingIp='$private_floatingIp > /home/ubuntu/info
