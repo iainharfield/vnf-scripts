6a447b4d7d1aecfba934923632399a99f2fd1339
iainharfield <iainharfield@users.noreply.github.com>
2018-10-04 22:38:47 +0100

    Update demo_stop.sh

--- demo_stop.sh
+++ demo_stop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "*******************************************"
-echo "* Stop...."
+echo "* Stop...."                         
 echo "* Description : $Description"
 echo "* mqttBroker = $mqttBroker"
 echo "* mqttPublishDelay = $mqttPublishDelay"
+echo "* mqttQueueName = $mqttQueueName"
 echo "* networkName: $networkName"
 echo "* hostname:    $hostname"
-echo "* private_floatingIp: $private_floatingIp "
+echo "* private_floatingIp: $private_floatingIp"
 echo "*******************************************"
 export STREAMBASE_HOME=/opt/tibco/sb-cep/10.3
 export JAVA_HOME=$STREAMBASE_HOME/jdk
