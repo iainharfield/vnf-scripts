1444750b57971235222d3e9d387a7ca686b113ea
iainharfield <iainharfield@users.noreply.github.com>
2018-10-04 22:36:54 +0100

    Update demo_configure.sh

--- demo_configure.sh
+++ demo_configure.sh
@@ -5,5 +5,9 @@ echo " networkName: $networkName"
 echo " hostname:    $hostname"
 echo " private_floatingIp: $private_floatingIp "
 echo "*******************************************"
+export STREAMBASE_HOME=/opt/tibco/sb-cep/10.3
+export JAVA_HOME=$STREAMBASE_HOME/jdk
+export TIBCO_EP_HOME=$STREAMBASE_HOME
+export PATH=/opt/tibco/sb-cep/10.3/jdk/bin:/opt/tibco/sb-cep/10.3/bin:/opt/tibco/sb-cep/10.3/distrib/tibco/bin:/opt/tibco/sb-cep/10.3/sdk/mvn/bin:$PATH
 
 echo 'private='$private > /home/ubuntu/info-configure
